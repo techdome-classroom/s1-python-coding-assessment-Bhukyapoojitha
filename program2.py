7f0fad25186761327bcafdeecbe3a6af93143a5c
Bhukyapoojitha <134996936+Bhukyapoojitha@users.noreply.github.com>
2024-11-11 14:14:00 +0530

    Add files via upload

--- program2.py
+++ program2.py
@@ -1,5 +1,38 @@
 def decode_message( s: str, p: str) -> bool:
 
 # write your code here
-  
-        return False
+
+    
+    i, j = 0, 0
+    star_idx = -1  
+    match_idx = 0  
+
+    while i < len(s):
+        if j < len(p) and (p[j] == '?' or s[i] == p[j]):
+
+            i += 1
+            j += 1
+        elif j < len(p) and p[j] == '*':
+           
+            star_idx = j
+            match_idx = i
+            j += 1
+        elif star_idx != -1:
+          
+            j = star_idx + 1
+            match_idx += 1
+            i = match_idx
+        else:
+            return False
+
+    while j < len(p) and p[j] == '*':
+        j += 1
+
+ 
+    return j == len(p)
+
+
+print(decode_message("aa", "a"))  # False
+print(decode_message("aa", "*"))  # True
+print(decode_message("cb", "?a"))  # False
+print(decode_message("aa", "a*"))  # True
